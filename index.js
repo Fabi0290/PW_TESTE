fb465f495eac568052efc4adee70ad278542b88e
Fábio <121135484+Fabi0290@users.noreply.github.com>
2024-06-19 21:57:48 +0100

    teste4

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4242;
+const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/frontend/TRAB_PRAT1.html');
-  });
+  res.sendFile(path.resolve(__dirname, 'frontend', 'TRAB_PRAT1.html'));
+});
 
 app.listen(port, () => {
- console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
