8f80aa2565e14b8c9f4445934679c2d9f369d465
Fábio <121135484+Fabi0290@users.noreply.github.com>
2024-06-19 22:03:08 +0100

    teste5

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = process.env.PORT || 4242;
 const path = require('path');
 
+// Serve static files from the frontEnd directory
+app.use(express.static(path.join(__dirname, 'frontEnd')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'TRAB_PRAT1.html'));
+  res.sendFile(path.resolve(__dirname, 'frontEnd', 'TRAB_PRAT1.html'));
 });
 
 app.listen(port, () => {
